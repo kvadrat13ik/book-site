5ee711758fef71cc90674d3850ec670ec37c96be
kvadrat13ik <kvadrat13ik@gmail.com>
2026-04-09 06:01:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ fetch('content.json')
 .then(res => res.json())
 .then(data => {
     const book = $('#book-container');
+    
+ data.forEach((page, index) => {
+    // Определяем, обложка это или обычная страница
+    const isCover = index === 0 || index === data.length - 1;
+    const className = isCover ? 'page cover' : 'page';
 
-    data.forEach(page => {
-        // Мы оставляем здесь только самое нужное, 
-        // остальное (размеры, прозрачность) подтянется из CSS автоматически
-        const div = $('<div/>', { class: 'page' }).css({
-            'background-image': `url('${page.texture}')`
-        });
-        book.append(div);
+    const div = $('<div/>', { class: className }).css({
+        'background-image': `url('${page.texture}')`
     });
+    book.append(div);
+});
 
     book.turn({
         width: 900,         // ИСПРАВЛЕНО: Теперь совпадает с CSS (450+450)
