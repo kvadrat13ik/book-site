b5a645a83c8e16907b1b07bcd3a0ebd1968954e3
kvadrat13ik <kvadrat13ik@gmail.com>
2026-04-09 05:02:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ fetch('content.json')
     const book = $('#book-container');
 
     data.forEach(page => {
-        const div = $('<div/>', {class: 'page'}).css({
-            'background-image': `url('${page.texture}')`,
-            'background-size': 'cover'
+        // Мы оставляем здесь только самое нужное, 
+        // остальное (размеры, прозрачность) подтянется из CSS автоматически
+        const div = $('<div/>', { class: 'page' }).css({
+            'background-image': `url('${page.texture}')`
         });
         book.append(div);
     });
 
     book.turn({
-        width: 800,
-        height: 500,
-        display: 'double',  // две страницы одновременно
+        width: 900,         // ИСПРАВЛЕНО: Теперь совпадает с CSS (450+450)
+        height: 600,        // ИСПРАВЛЕНО: Теперь совпадает с CSS
+        display: 'double',  
         autoCenter: true,
-        elevation: 50,      // тень и сгиб
-        gradients: true,
+        elevation: 0,       // ИСПРАВЛЕНО: 0 убирает лишние белые тени при перелистывании
+        gradients: false,   // ИСПРАВЛЕНО: false убирает стандартный серый градиент Turn.js
         duration: 1000
     });
 });
